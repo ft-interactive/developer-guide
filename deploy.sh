a2ff98732fa9dfac2e4a077dad6a9f9c3be05cc9
Callum Locke <callum.locke@gmail.com>
2015-09-30 16:50:40 +0100

    typo

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo ""
 
 # skip deployment if this isn't the production branch
 echo "\$TRAVIS_BRANCH: $TRAVIS_BRANCH"
-echo "\$TRAVIS_PULL_REQUEST: $TRAVIS_BRANCH"
+echo "\$TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo ""
 
 if [[ $TRAVIS_PULL_REQUEST || $TRAVIS_BRANCH != "production" ]]; then
